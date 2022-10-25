409c1558ff45ce32663dcacc372f636adcb25b2f
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-10-25 04:12:06 +0000

    add comments

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,23 @@
-const args = process.argv.slice(2);
-const request = require("request");
-const fs = require("fs");
+const args = process.argv.slice(2); //enable command line arguments
+const request = require("request"); // allows request
+const fs = require("fs"); 
 
+//make request
 request(args[0], (err, response) => {
   if (err) {
     console.log(err);
   }
+  //writes data from URL to a chosen local file
   fs.writeFile(args[1], JSON.stringify(response), (err) => {
     if (err) {
       console.log(err);
     }
+    //gets file size
     fs.stat(args[1], (err, stats) => {
       if (err) {
         console.log(err);
       }
+      //save file size as variable
       const bytes = stats.size;
       console.log(`Downloaded and saved ${bytes} bytes to ${args[1]}`);
     });
